ccdd268090105f413e9607ed00289b609bb0aa3c
Federico Veronesi <federicoveronesi6@gmail.com>
2023-03-16 17:01:37 +0100

    feat: Opening details

--- Bartender/Scenes/CocktailsListView.swift
+++ Bartender/Scenes/CocktailsListView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct CocktailsListView: View {
+    @State var presentDetails: Bool = false
+
     var body: some View {
-        Text("Cocktails List View")
+        NavigationStack {
+            Button("to Cocktail Details") {
+                presentDetails = true
+            }
+            .navigationDestination(isPresented: $presentDetails) {
+                CocktailDetailsView()
+            }
+        }
     }
+}
+
+struct Cocktail {
+    let name: String
 }
 
 struct CocktailsListView_Previews: PreviewProvider {
