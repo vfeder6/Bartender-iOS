b92e0f6000b48c9f0295d69b58962564e3d345c5
Federico Veronesi <federicoveronesi6@gmail.com>
2023-03-16 16:22:30 +0100

    feat: Project created

---
+++ Bartender/BartenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BartenderApp.swift
+//  Bartender
+//
+//  Created by Federico Veronesi on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BartenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
