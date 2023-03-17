1c420be96546ead8a87c3951c16e6e210c4fed3e
Federico Veronesi <federicoveronesi6@gmail.com>
2023-03-17 01:26:57 +0100

    feat: Decoding

--- Bartender/Scenes/CocktailsListView.swift
+++ Bartender/Scenes/CocktailsListView.swift
@@ -4,37 +4,52 @@ struct CocktailsListView: View {
     @StateObject var viewModel: CocktailsListViewModel
 
     var body: some View {
-        NavigationStack {
-            ScrollView(showsIndicators: false) {
-                ForEach(viewModel.state.cocktails) { cocktail in
-                    NavigationLink(value: cocktail) {
-                        HStack {
-                            VStack {
-                                HStack(spacing: 0) {
-                                    Text(cocktail.name).font(.system(size: 24))
-                                    Spacer(minLength: 0)
-                                }
-                                HStack(spacing: 0) {
-                                    Text(cocktail.category)
-                                    Spacer(minLength: 0)
-                                }
-                            }
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.vertical, 20).padding(.horizontal)
-                        .contentShape(Rectangle())
-                    }.buttonStyle(.plain)
-                    Divider()
-                }
+        ScrollView(showsIndicators: false) {
+            ForEach(viewModel.state.cocktails) { cocktail in
+                NavigationLink(value: cocktail) {
+                    row(for: cocktail)
+                }.buttonStyle(.plain)
+                Divider()
             }
-            .navigationTitle("Drinks")
-                .navigationDestination(for: Cocktail.self) { cocktail in
-                    CocktailDetailsView()
-                }
+        }
+        .navigationStack(withTitle: "Drinks", forDestination: Cocktail.self) { cocktail in
+            CocktailDetailsView()
         }
         .task {
             await viewModel.fetch()
+        }
+    }
+
+    private func row(for cocktail: Cocktail) -> some View {
+        HStack {
+            VStack {
+                HStack(spacing: 0) {
+                    Text(cocktail.name).font(.system(size: 24))
+                    Spacer(minLength: 0)
+                }
+                HStack(spacing: 0) {
+                    Text(cocktail.category.rawValue)
+                    Spacer(minLength: 0)
+                }
+            }
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray)
+        }
+        .padding(.vertical, 20).padding(.horizontal)
+        .contentShape(Rectangle())
+    }
+}
+
+private extension View {
+    func navigationStack<Destination, Content>(
+        withTitle title: String,
+        forDestination data: Destination.Type,
+        _ destinationContent: @escaping (Destination) -> Content
+    ) -> some View where Destination: Hashable, Content: View {
+        NavigationStack {
+            self
+                .navigationTitle(title)
+                .navigationDestination(for: data, destination: destinationContent)
         }
     }
 }
--- Bartender/Services/CocktailsListService.swift
+++ Bartender/Services/CocktailsListService.swift
@@ -26,11 +26,18 @@ struct CocktailsListServicePreview: CocktailsListServiceProtocol {
 
     func perform() async -> [Cocktail] {
         [
-            .init(id: "1", name: "Margarita", category: "Cocktail", glass: "Boh"),
-            .init(id: "2", name: "Margarita", category: "Party drink", glass: "Boh"),
-            .init(id: "3", name: "Margarita", category: "Classical drink", glass: "Boh"),
-            .init(id: "4", name: "Margarita", category: "Cocktail", glass: "Boh"),
-            .init(id: "5", name: "Margarita", category: "Cocktail", glass: "Boh")
+            .init(id: "1", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "2", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "3", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "4", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "5", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "6", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "7", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "8", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "9", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "10", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "11", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
+            .init(id: "12", name: "Test", category: .cocktail, glass: .highballGlass, isAlcoholic: true, ibaCategory: .contemporaryClassic, instructions: "test instructions"),
         ]
     }
 }
@@ -64,13 +71,96 @@ struct CocktailsListResponse: Decodable {
 struct Cocktail: Decodable, Hashable, Identifiable {
     let id: String
     let name: String
-    let category: String
-    let glass: String
+    let category: DrinkCategory
+    let glass: Glass
+    let isAlcoholic: Bool
+    let ibaCategory: IBACategory?
+    let instructions: String
+
+    init(
+        id: String,
+        name: String,
+        category: DrinkCategory,
+        glass: Glass,
+        isAlcoholic: Bool,
+        ibaCategory: IBACategory?,
+        instructions: String
+    ) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.glass = glass
+        self.isAlcoholic = isAlcoholic
+        self.ibaCategory = ibaCategory
+        self.instructions = instructions
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = values.decodeDebug(String.self, forKey: .id)
+        name = values.decodeDebug(String.self, forKey: .name)
+        category = values.decodeDebug(DrinkCategory.self, forKey: .category)
+        glass = values.decodeDebug(Glass.self, forKey: .glass)
+        isAlcoholic = values.decodeDebug(String.self, forKey: .isAlcoholic) == "Alcoholic"
+        ibaCategory = values.decodeDebug(IBACategory.self, forKey: .ibaCategory)
+        instructions = values.decodeDebug(String.self, forKey: .instructions)
+    }
 
     enum CodingKeys: String, CodingKey {
         case id = "idDrink"
         case name = "strDrink"
         case category = "strCategory"
         case glass = "strGlass"
+        case isAlcoholic = "strAlcoholic"
+        case ibaCategory = "strIBA"
+        case instructions = "strInstructions"
+    }
+
+    enum IBACategory: String, Decodable, Unknownable {
+        case contemporaryClassic = "Contemporary Classics"
+        case unforgettables = "Unforgettables"
+        case unknown
+    }
+
+    enum DrinkCategory: String, Decodable, Unknownable {
+        case cocktail = "Cocktail"
+        case ordinaryDrink = "Ordinary Drink"
+        case punchPartyDrink = "Punch / Party Drink"
+        case unknown
+    }
+
+    enum Glass: String, Decodable, Unknownable {
+        case highballGlass = "Highball glass"
+        case oldFashionedGlass = "Old-fashioned glass"
+        case cocktailGlass = "Cocktail glass"
+        case copperMug = "Copper Mug"
+        case whiskeyGlass = "Whiskey Glass"
+        case unknown
+    }
+}
+
+extension KeyedDecodingContainer {
+    func decodeDebug<T>(_ type: T.Type, forKey key: Self.Key) -> T where T: Decodable & Unknownable {
+        do {
+            return try decode(type, forKey: key)
+        } catch {
+            if case .dataCorrupted(let context) = (error as? DecodingError) {
+                print("DECODING:", context.debugDescription)
+            } else {
+                print("DECODING: UNKNOWN", error)
+            }
+            return .unknown
+        }
+    }
+}
+
+protocol Unknownable {
+    static var unknown: Self { get }
+}
+
+extension String: Unknownable {
+    static var unknown: String {
+        "Unknown"
     }
 }
