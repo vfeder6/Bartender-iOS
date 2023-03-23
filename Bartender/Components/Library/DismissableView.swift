9ef293d276981980e6fd1d58b58a96ac81aa51f3
Federico Veronesi <federicoveronesi6@gmail.com>
2023-03-23 09:20:28 +0100

    feat: Navigation stack embedding for button

--- Bartender/Components/Library/DismissableView.swift
+++ Bartender/Components/Library/DismissableView.swift
@@ -2,16 +2,40 @@ import SwiftUI
 
 struct DismissableView<Content: View>: View {
     @Binding var isPresented: Bool
+    let embedInNavigationStack: Bool
     let content: () -> Content
 
     var body: some View {
+        if embedInNavigationStack {
+            navigationStack
+        } else {
+            zStack
+        }
+    }
+
+    private var navigationStack: some View {
+        NavigationStack {
+            content()
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        button
+                    }
+                }
+        }
+    }
+
+    private var zStack: some View {
         ZStack {
             content()
-            Button(action: { isPresented = false }, label: dismissLabel)
+            button
                 .padding(.trailing)
                 .horizontalAlignment(.trailing)
                 .verticalAlignment(.top)
         }
+    }
+
+    private var button: some View {
+        Button(action: { isPresented = false }, label: dismissLabel)
     }
 
     private func dismissLabel() -> some View {
@@ -30,27 +54,28 @@ struct DismissableView<Content: View>: View {
 }
 
 extension View {
-    func dismissable(isPresented: Binding<Bool>) -> some View {
-        DismissableView(isPresented: isPresented) {
+    func dismissable(isPresented: Binding<Bool>, embedInNavigationStack: Bool = false) -> some View {
+        DismissableView(isPresented: isPresented, embedInNavigationStack: embedInNavigationStack) {
             self
         }
     }
 }
 
 extension View {
-    func dismissableFullScreenCover<T: Identifiable>(
-        item: Binding<T?>,
+    func dismissableFullScreenCover<Item: Identifiable>(
+        item: Binding<Item?>,
+        embedInNavigationStack: Bool = false,
         onDismiss: (() -> Void)? = nil,
-        content: @escaping (T) -> some View
+        content: @escaping (Item) -> some View
     ) -> some View {
         self.fullScreenCover(item: item, onDismiss: onDismiss) { identifiable in
-            content(identifiable).dismissable(isPresented: .optional(item))
+            content(identifiable).dismissable(isPresented: .optional(item), embedInNavigationStack: embedInNavigationStack)
         }
     }
 }
 
 extension Binding where Value == Bool {
-    static func optional<T: Identifiable>(_ item: Binding<T?>) -> Self {
+    static func optional<Item: Identifiable>(_ item: Binding<Item?>) -> Self {
         .init(get: { item.wrappedValue != nil }, set: { if !$0 { item.wrappedValue = nil } })
     }
 }
