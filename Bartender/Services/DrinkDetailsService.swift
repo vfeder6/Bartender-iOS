1034c0210df4d19fa4641f6a3dd3efcc67043b2a
Federico Veronesi <federicoveronesi6@gmail.com>
2023-03-25 23:13:50 +0100

    feat: Services change

--- Bartender/Services/DrinkDetailsService.swift
+++ Bartender/Services/DrinkDetailsService.swift
@@ -4,22 +4,21 @@ import Networking
 struct DrinkDetailsService: Service {
     let networkClient: NetworkClient<DrinkDetailsResponse>
 
-    static var live: Self {
-        .init(networkClient: .live)
-    }
-
-    func perform(body: Encodable?, queryItems: [URLQueryItem]) async -> Result<DrinkDetailsResponse, NetworkError> {
-        await networkClient.responseResult(from: "lookup.php", queryItems: queryItems)
-    }
-
     func perform(drinkID: String) async -> Result<Drink, NetworkError> {
-        await perform(body: nil, queryItems: [.init(name: "i", value: drinkID)])
-            .flatMap { response in
-                guard let drink = response.drinks.first else {
-                    return .failure(.notDecodableData(model: type(of: response), json: nil))
-                }
-                return .success(drink)
+        await networkClient.responseResult(
+            from: "lookup.php",
+            queryItems: [.init(name: "i", value: drinkID)],
+            body: nil,
+            method: .get,
+            additionalHeaders: [:],
+            expectedStatusCode: 200
+        )
+        .flatMap { response in
+            guard let drink = response.drinks.first else {
+                return .failure(.notDecodableData(model: type(of: response), json: nil))
             }
+            return .success(drink)
+        }
     }
 }
 
--- Bartender/Services/DrinksListService.swift
+++ Bartender/Services/DrinksListService.swift
@@ -4,17 +4,16 @@ import Networking
 struct DrinksListService: Service {
     let networkClient: NetworkClient<DrinksListResponse>
 
-    static var live: Self {
-        .init(networkClient: .live)
-    }
-
-    func perform(body: Encodable?, queryItems: [URLQueryItem]) async -> Result<DrinksListResponse, NetworkError> {
-        await networkClient.responseResult(from: "filter.php", queryItems: queryItems)
-    }
-
     func perform() async -> Result<[DrinkSummary], NetworkError> {
-        await perform(body: nil, queryItems: [.init(name: "a", value: "Alcoholic")])
-            .map(\.drinkSummaries)
+        await networkClient.responseResult(
+            from: "filter.php",
+            queryItems: [.init(name: "a", value: "Alcoholic")],
+            body: nil,
+            method: .get,
+            additionalHeaders: [:],
+            expectedStatusCode: 200
+        )
+        .map(\.drinkSummaries)
     }
 }
 
